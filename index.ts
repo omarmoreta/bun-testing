7f7353ca6646be1f0416c71bc7a5f51c777dbf85
Omar Moretea <o.moreta@gmail.com>
2026-02-22 21:00:28 -0800

    added html page to http server

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import figlet from "figlet";
+import index from "./index.html";
 
 const server = Bun.serve({
   port: 3000,
   routes: {
-    "/": () => new Response("Bun!"),
+    "/": index,
     "/figlet": () => {
       const body = figlet.textSync("Bun!");
       return new Response(body);
